e4c1b87a4aa4a514bc7fbac31f9323c60d9cb553
javanhut <javan.hutchinson18@gmail.com>
2025-05-09 17:50:30 -0400

    feat: added example class

--- python_test.py
+++ python_test.py
@@ -2,3 +2,24 @@ from typing import Dict, Union, List, Optional
 from fastapi import FastApi
 import requests 
 
+
+
+
+
+
+class TestClass:
+
+    def __init__(self):
+        self.class_type: str = "Example class"
+
+    def not_important(self, example_params: Union[None, str]):
+        pass
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    test_class = TestClass()
